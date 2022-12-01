395b501e6d570fb97210d208d0d1a4111608725d
sohailtanveer01 <sohailtanveer769@gmail.com>
2022-12-01 17:15:58 +0530

    updated client routes

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
             <Route path="/" element={<LoginPage />} />
             <Route
               path="/home"
-              element={<HomePage />}
+              element={isAuth ? <HomePage /> : <Navigate to="/" />}
             />
             <Route
               path="/profile/:userId"
