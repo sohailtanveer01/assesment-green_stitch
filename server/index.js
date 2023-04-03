bd52bb2e34592c19d330030879a1f4ce843ab138
sohailtanveer01 <sohailtanveer769@gmail.com>
2023-04-03 16:23:59 +0530

    login-signup assesment

--- server/index.js
+++ server/index.js
@@ -31,6 +31,12 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+app.use(express.static(path.join(__dirname, "../build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../build/index.html"));
+});
+
 
 /* FILE STORAGE */
 const storage = multer.diskStorage({
